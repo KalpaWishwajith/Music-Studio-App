b8c57cc58d6c6125cb833f9eaef908d9b43f65d0
KalpaWishwajith <kalpawishwajith00000@gmail.com>
2025-01-04 12:38:36 +0530

    Remove console log from getPlaylist function in PlaylistServices

--- services/PlaylistServices.js
+++ services/PlaylistServices.js
@@ -5,7 +5,6 @@ export const getPlaylist = async () => {
     const response = await axios.get(
       "https://itunes.apple.com/search?term=ed+sheeran&entity=song"
     );
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
